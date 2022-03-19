927e728ae9b66e54070b0780f94004e931e0ed3b
iamhotcenplotz <236381065@qq.com>
2022-03-19 21:32:45 +0800

    chong xin gao

---
+++ config_files/workers.md
@@ -0,0 +1,8 @@
+# Hadoop Cluster workers config
+
+- Be careful with space. Workers config file is sensitive with space and empty line.
+```shell
+server1
+server2
+server3
+```
---
+++ my_env.sh
@@ -0,0 +1,23 @@
+#JAVA_HOME
+export JAVA_HOME=/opt/modules/jdk1.8.0_211
+export PATH=$PATH:$JAVA_HOME/bin
+
+
+#HADOOP_HOME
+export HADOOP_HOME=/opt/modules/hadoop-3.2.2
+export PATH=$PATH:$HADOOP_HOME/bin
+export PATH=$PATH:$HADOOP_HOME/sbin
+
+#XSYNC_HOME
+export XSYNC_HOME=/home/hadoop/bin
+export PATH=$PATH:$XSYNC_HOME
+
+
+#KAFKA_HOME
+export KAFKA_HOME=/opt/modules/kafka
+export PATH=$PATH:$KAFKA_HOME/bin
+
+
+#ZOOKEEPER_HOME
+export ZOOKEEPER_HOME=/opt/modules/zookeeper-3.7.0-bin
+export PATH=$PATH:$ZOOKEEPER_HOME/bin
