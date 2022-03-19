ead4fcb10fc15ffe72f25c90b764e40950166fc1
iamhotcenplotz <236381065@qq.com>
2022-03-20 05:59:04 +0800

    add python method to operate hdfs remotely. I mean outsite of the firewall of cluster

--- HadoopPython/my_hdfs.py
+++ HadoopPython/my_hdfs.py
@@ -10,7 +10,6 @@ Upload
 
 """
 
-
 """
 Example config from hdfs home: https://hdfscli.readthedocs.io/en/latest/quickstart.html
 client = InsecureClient('http://host:port', user='ann')
@@ -20,8 +19,23 @@ config = {
     'url': HadoopConfig.url,
     'user': HadoopConfig.user
 }
+"""
+sample config
+config = {
+    'url':'http://host:14000', # must be NameNode host
+    'user':'user'
+}
+"""
 
 client = hdfs.InsecureClient(**config)
-print(client.makedirs('/test_api2'))
-client.upload(hdfs_path='/test_api2/',local_path='./Hadoop_API.md',n_threads=1)
+# Make folder
+client.makedirs('/test_api2')
 
+# upload file
+client.upload(hdfs_path='/test_api2/', local_path='./Hadoop_API.md', n_threads=1)
+
+# download file
+client.download(hdfs_path='/ods_news/ods_news_east.txt', local_path='./', n_threads=3)
+
+# check hdfs folders
+client.list(hdfs_path='/')
