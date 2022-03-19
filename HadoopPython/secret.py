001e9ecaa622fc204b1957726ed3f11403cfbdca
iamhotcenplotz <236381065@qq.com>
2022-03-20 05:42:06 +0800

    a

--- HadoopPython/secret.py
+++
@@ -1,7 +0,0 @@
-class HadoopConfig:
-    url = 'http://47.113.227.185:14000'
-    user = 'hadoop'
-
-# hdfs --daemon stop httpfs
-#
-# curl -sS 'http://server1:14000/webhdfs/v1?op=gethomedirectory&user.name=hdfs'
